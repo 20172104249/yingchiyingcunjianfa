b40351ea52e22fcb4b836b8be0c71c8acb01cd92
20172104249 <2980796841@qq.com>
2018-05-29 15:28:31 +0800

    完善程序
    考虑0,0-0,1的问题

--- yingchiyingcunjianfa/yingchiyingcunjianfa.cpp
+++ yingchiyingcunjianfa/yingchiyingcunjianfa.cpp
@@ -10,38 +10,46 @@ private:
 	int feet;
 	int inches;
 public:
-	void setvalue(int foot,int inch)
+	void setvalue(int foot, int inch)
 	{
 		feet = foot;
 		inches = inch;
 	}
 	void display()
 	{
-		cout << feet <<" feet "<< inches <<"inches"<< endl;
+		cout << feet << " feet " << inches << "inches" << endl;
 	}
 	CFeet operator +(CFeet&o)
 	{
-		CFeet temp;
+		CFeet te;
 		inches = feet * 12 + inches;
 		o.inches = o.feet * 12 + o.inches;
-		temp.setvalue((inches + o.inches) / 12, (inches + o.inches) % 12);
-		return temp;
+		te.setvalue((inches + o.inches) / 12, (inches + o.inches) % 12);
+		return te;
 	}
 	CFeet operator -(CFeet&o)
 	{
 		CFeet temp;
 		if (feet < 0)
 		{
-			inches = -1*inches;
-			inches = feet * 12 + inches;
-			o.inches = o.feet * 12 + o.inches;
-			temp.setvalue((inches - o.inches) / 12, labs(inches - o.inches ) % 12);
-		}
-		else 
-		{
+			inches = -1 * inches;
 			inches = feet * 12 + inches;
 			o.inches = o.feet * 12 + o.inches;
 			temp.setvalue((inches - o.inches) / 12, labs(inches - o.inches) % 12);
+		}
+		else
+		{
+			inches = feet * 12 + inches;
+			o.inches = o.feet * 12 + o.inches;
+			if ((inches - o.inches) / 12 == 0)
+			{
+				temp.setvalue((inches - o.inches) / 12, (inches - o.inches) % 12);
+			}
+			else
+			{
+				temp.setvalue((inches - o.inches) / 12, labs(inches - o.inches) % 12);
+			}
+
 		}
 
 		return temp;
@@ -50,13 +58,16 @@ public:
 
 int main()
 {
-	CFeet A, B, C;
-	A.setvalue(5, 5);
-	B.setvalue(2, 11);
-	C = A + B;
-	C.display();
+	CFeet A, B, C, D;
+	A.setvalue(2, 0);
+	B.setvalue(3, 1);
+
 	C = A - B;
 	C.display();
-    return 0;
-}
 
+	A.setvalue(2, 0);
+	B.setvalue(3, 1);
+	D = A + B;
+	D.display();
+	return 0;
+}
