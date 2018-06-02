0992fa6036e120cf7e61d82c9ca0adac41d40986
20172104249 <2980796841@qq.com>
2018-06-02 16:38:17 +0800

    修改一些问题
    如果 inches是负数的问题计算减法
    加法没问题

--- yingchiyingcunjianfa/yingchiyingcunjianfa.cpp
+++ yingchiyingcunjianfa/yingchiyingcunjianfa.cpp
@@ -17,7 +17,7 @@ public:
 	}
 	void display()
 	{
-		cout << feet << " feet " << inches << "inches" << endl;
+		cout << feet << " feet " << inches << " inches " << endl;
 	}
 	CFeet operator +(CFeet&o)
 	{
@@ -29,27 +29,26 @@ public:
 	}
 	CFeet operator -(CFeet&o)
 	{
+		int i;
+		inches = inches + feet * 12;
+		o.inches = o.inches + o.feet * 12;
+		i = inches - o.inches;
 		CFeet temp;
-		if (feet < 0)
+		if (i<0)
 		{
-			inches = -1 * inches;
-			inches = feet * 12 + inches;
-			o.inches = o.feet * 12 + o.inches;
-			temp.setvalue((inches - o.inches) / 12, labs(inches - o.inches) % 12);
-		}
-		else
-		{
-			inches = feet * 12 + inches;
-			o.inches = o.feet * 12 + o.inches;
-			if ((inches - o.inches) / 12 == 0)
+			if (i <= -12)
 			{
-				temp.setvalue((inches - o.inches) / 12, (inches - o.inches) % 12);
+				i = -1 * i;
+				temp.setvalue(-1 * (i / 12), i % 12);
 			}
 			else
 			{
-				temp.setvalue((inches - o.inches) / 12, labs(inches - o.inches) % 12);
+				temp.setvalue(i / 12, (i % 12));
 			}
-
+		}
+		else
+		{
+			temp.setvalue(i / 12, i % 12);
 		}
 
 		return temp;
@@ -59,8 +58,8 @@ public:
 int main()
 {
 	CFeet A, B, C, D;
-	A.setvalue(2, 0);
-	B.setvalue(3, 1);
+	A.setvalue(-1, 1);
+	B.setvalue(-3, 1);
 
 	C = A - B;
 	C.display();
